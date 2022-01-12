ac6480a49c09ecba82e8eb932aa4d8f9d4bee80a
NastyTester <70013669+NastyTester@users.noreply.github.com>
2022-01-12 11:49:53 +0200

    Home eddited

--- el-proyecte-grande/el-proyecte-grande/ClientApp/src/App.js
+++ el-proyecte-grande/el-proyecte-grande/ClientApp/src/App.js
@@ -3,8 +3,6 @@ import { Route } from 'react-router';
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
 import { NotFound } from './components/NotFound';
 
 import './custom.css'
@@ -22,8 +20,6 @@ export default class App extends Component {
         <Layout />
         <Switch>
           <Route exact path='/' component={Home} />
-          <Route path='/counter' component={Counter} />
-          <Route path='/fetch-data' component={FetchData} />
           <Route path="*" component={NotFound} />
         </Switch>
       </Router>
