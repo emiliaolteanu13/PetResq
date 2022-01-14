e50aeaf3901521a6ac8edd95592d29a6ac2fd86c
NastyTester <70013669+NastyTester@users.noreply.github.com>
2022-01-14 09:31:02 +0200

    it is what it is...

--- el-proyecte-grande/el-proyecte-grande/ClientApp/src/components/Home.js
+++ el-proyecte-grande/el-proyecte-grande/ClientApp/src/components/Home.js
@@ -5,12 +5,29 @@ import * as actions from "../actions/posts";
 
 export class Home extends Component {
   static displayName = Home.name;
+  
 
   render () {
+    const mystyle = {
+      color: "white",
+      backgroundColor: "#BEBEDB",
+      marginTop: "0px",
+      padding: "10em",
+      display: "flex"
+    }
+
+    const linkStyle= {
+      backgroundColor: "#4985F4",
+      color:"#08032F",
+      fontSize: "20px",
+      padding: "3em",
+      margin: "10%",
+      borderRadius: "16px"
+    }
     return (
-      <div className='home'>
-      <Link to="/find">Adopt an Animal</Link>
-      <Link to="/create">Make a post</Link>
+      <div className='home' style={mystyle}>
+      <Link to="/find" style={linkStyle}>Adopt an Animal</Link>
+      <Link to="/create" style={linkStyle}>Make a post</Link>
       </div>
     );
   }
