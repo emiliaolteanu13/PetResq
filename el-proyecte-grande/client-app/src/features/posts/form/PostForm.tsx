1b1c0b687cd360a4e8bdd3cbe4668574da58e7c2
mihail-dev101 <73068128+mihail-dev101@users.noreply.github.com>
2022-01-19 12:55:07 +0200

    Added Select with options to PostForm

--- el-proyecte-grande/client-app/src/features/posts/form/PostForm.tsx
+++ el-proyecte-grande/client-app/src/features/posts/form/PostForm.tsx
@@ -10,6 +10,17 @@ interface Props {
 
 export default function PostForm({post: selectedPost, closeForm, createOrEdit}: Props) {
 
+    const petType = [
+        {key: 1, value: 1, text: "CAT"},
+        {key: 2, value: 2, text: "DOG"},
+        {key: 3, value: 3, text: "ALPACA"},
+        {key: 4, value: 4, text: "OTHER"}];
+
+    const statusType = [
+        {key: 1, value: 1, text: "LOST"},
+        {key: 2, value: 2, text: "FOUND"},
+        {key: 3, value: 3, text: "FOR ADOPTION"}];
+
     const initialState = selectedPost ?? {
         id:'',
         title: '',
@@ -37,7 +48,8 @@ export default function PostForm({post: selectedPost, closeForm, createOrEdit}: 
             <Form onSubmit={handleSubmit} autoComplete='off'>
                 <Form.Input placeholder = 'Title' value={post.title} name='title' onChange={handleInputChange}/>
                 <Form.TextArea placeholder = 'Description' value={post.description} name='description' onChange={handleInputChange}/>
-                {/*<Form.Select placeholder = "Animal type" options={post.petType}/>   de vazut maine exact cum facem cu options*/}
+                <Form.Select placeholder = "Animal type" options={petType} />
+                <Form.Select placeholder = "Post type" options={statusType} />
                 <Form.Input placeholder = 'Location' value={post.location} name='location' onChange={handleInputChange}/>
                 <Form.Input placeholder = 'Date' value={post.date} name='date' onChange={handleInputChange}/>
                 <Button floated='right' positive type='submit' content='Submit'/>
