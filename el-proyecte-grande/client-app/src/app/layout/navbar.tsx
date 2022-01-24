3673aa37e64b6d40bdb87e315e10dc00fb86b593
NastyTester <70013669+NastyTester@users.noreply.github.com>
2022-01-24 21:24:37 +0200

    designed the navbar

--- el-proyecte-grande/client-app/src/app/layout/navbar.tsx
+++ el-proyecte-grande/client-app/src/app/layout/navbar.tsx
@@ -4,18 +4,18 @@ import { Button, Container, Menu } from 'semantic-ui-react';
 
 
 export default function NavBar() {
+    
+    
+    
 
     return (
         <Menu inverted fixed="top">
             <Container>
                 <Menu.Item as={NavLink} to='/' exact header>
-                    <img src="/assets/logo.png" alt="logo" style={{marginRight: '10px'}} />
-                    PetResQ
+                    <img src="/assets/logo.png"  alt="logo" id='Logo' style={{marginRight: '15px', height: '10em', width: 'auto'}} />
                 </Menu.Item>
                 <Menu.Item as={NavLink} to='/posts' name='Posts' />
-                <Menu.Item>
-                    <Button as={NavLink} to='/createPost' positive content='Create Post' />
-                </Menu.Item>
+                <Menu.Item as={NavLink} to='/createPost' positive content='Create Post' />
             </Container>
         </Menu>
     )
