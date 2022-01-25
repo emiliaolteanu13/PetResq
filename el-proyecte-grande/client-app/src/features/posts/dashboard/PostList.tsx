f82c1eb831e7b24b7593af29798c6bb45524983f
emiliaolteanu13 <emiliaolteanu13@yahoo.com>
2022-01-25 14:35:57 +0200

    post details page startde

--- el-proyecte-grande/client-app/src/features/posts/dashboard/PostList.tsx
+++ el-proyecte-grande/client-app/src/features/posts/dashboard/PostList.tsx
@@ -1,8 +1,8 @@
 import { observer } from "mobx-react-lite";
-import React, { SyntheticEvent, useState } from "react";
-import { Link } from "react-router-dom";
-import { Button, Item, Label, Segment } from "semantic-ui-react";
+import React, { Fragment } from "react";
+import { Item, Segment } from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
+import PostListItem from "./PostListItem";
 
 
 
@@ -12,44 +12,15 @@ import { useStore } from "../../../app/stores/store";
 export default observer(function PostList() {
 
     const {postStore} = useStore();
-    const { deletePost, postsByDate, loading } = postStore;
-    const [target, setTarget] = useState('');
-
-    function handlePostDelete(e: SyntheticEvent<HTMLButtonElement>, id: string) {
-        setTarget(e.currentTarget.name);
-        deletePost(id);
-    }
+    const {postsByDate} = postStore;
 
     
 
     return (
-        <Segment>
-            <Item.Group divided>
+            <Fragment>
                 {postsByDate.map(post => (
-                    <Item key={post.id}>
-                        <Item.Content>
-                            <Item.Header as='a'>{post.title}</Item.Header>
-                            <Item.Meta>{post.date}</Item.Meta>
-                            <Item.Description>
-                                <div>{post.description}</div>
-                                <div>{post.location}</div>
-                            </Item.Description>
-                            <Item.Extra>
-                                <Button as={Link} to={`/posts/${post.id}`} floated='right' content='View' color='blue'/>
-                                <Button 
-                                    name={post.id}
-                                    loading={loading && target === post.id} 
-                                    onClick={(e)=> handlePostDelete(e, post.id)} 
-                                    floated='right' 
-                                    content='Delete' 
-                                    color='red'/>
-                                <Label basic content={post.petType} />
-                                <Label basic content={post.statusType} />
-                            </Item.Extra>
-                        </Item.Content>
-                    </Item>
+                    <PostListItem key={post.id}  post = {post}></PostListItem>
                 ))}
-            </Item.Group>
-        </Segment>
+            </Fragment>
     )
 })
---
+++ el-proyecte-grande/client-app/src/features/posts/details/PostDetailedSidebar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function PostDetailedSidebar(){
+    return(
+        <h1>Sidebar</h1>
+    )
+}
