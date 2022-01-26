8ca02af083f46d019c7efd13329e740c6a887feb
NastyTester <70013669+NastyTester@users.noreply.github.com>
2022-01-26 15:41:03 +0200

    home page 2nd try

--- el-proyecte-grande/client-app/src/features/home/HomePage.tsx
+++ el-proyecte-grande/client-app/src/features/home/HomePage.tsx
@@ -1,46 +1,31 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Container } from "semantic-ui-react";
-//@ts-ignore
-import {Helmet} from "react-helmet";
 import './homepage.css';
-//@ts-ignore
-import ScriptTag from 'react-script-tag';
+import simpleParallax from 'simple-parallax-js';
+import background from './background.png'
 
 
 export default function HomePage() {
     
     // const[rellax, setRellax] = useState();
-    // useEffect(() => {
-    //     setRellax(new Rellax('.rellax',{horizontal: true, vertical: false, speed: 1.5}));
-    // })
+    useEffect(() => {
+        var image = document.getElementsByClassName('thumbnail');
+        new simpleParallax(image, {
+            orientation: 'right',
+            transition: 'true',
+        });
+    })
     
     return (
         <>
-            <aside className="helper">Scroll sideways</aside>
-            <main>
-                <figure className="rellax">
-                    <img src="https://source.unsplash.com/aiyBwbrWWlo" alt="Me coding" />
-                    <figcaption className="rellax" data-rellax-speed="1">Me coding</figcaption>
-                </figure>
-  
-                <figure className="rellax">
-                    <img src="https://source.unsplash.com/EaJm0ZfO84U" alt="Bronte Beach, Australia" />
-                    <figcaption className="rellax" data-rellax-speed="1">Bronte Beach, Australia</figcaption>
-                </figure>
-  
-                <figure className="rellax">
-                    <img src="https://source.unsplash.com/e9rkZKjdlSc" alt="The moon" />
-                    <figcaption className="rellax" data-rellax-speed="1">The moon</figcaption>
-                </figure>
-            </main>
+                    <div className="home">
 
-            {/* <ScriptTag src="https://cdn.rawgit.com/dixonandmoe/rellax/master/rellax.min.js"/> */}
-            <Helmet>
-                <script src="https://cdn.rawgit.com/dixonandmoe/rellax/master/rellax.min.js"></script>
-            </Helmet>
-            
+                    </div>
+
         </>
+
+            
     )
 }
 
