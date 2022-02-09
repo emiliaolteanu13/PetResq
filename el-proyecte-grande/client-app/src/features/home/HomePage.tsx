30b25c9fe14e3fdea05c9eebb655e4ff97d2f50a
NastyTester <70013669+NastyTester@users.noreply.github.com>
2022-02-09 16:40:57 +0200

    worked on homepage design

--- el-proyecte-grande/client-app/src/features/home/HomePage.tsx
+++ el-proyecte-grande/client-app/src/features/home/HomePage.tsx
@@ -4,6 +4,10 @@ import { Container, Button } from "semantic-ui-react";
 import './homepage.css';
 import Sky from './Sky.png';
 import Field from './Field.png';
+import Footer from "../../app/layout/Footer";
+import Cat from './cat.png';
+import Dog from './dog.png';
+import Cat2 from './cat2.png';
 
 
 export default function HomePage() {
@@ -11,28 +15,30 @@ export default function HomePage() {
 
     
     return (
-        <>
             <div className="wrapper">
-                <header>
+                <div className="header">
                     <img src={Sky} className="background" />
                     <img src={Field} className="foreground" />
                     <h1 className="title">Welcome</h1>
-                </header>
+                </div>
                 <div className="option1">
                     Are you here to adopt a pet?
                     <Link to='/posts' > Adopt</Link>
+                    <img src={Cat} className="option1img"/>
                 </div>
                 <div className="option2">
+                    <img src={Dog} className="option2img" />
                     Did you furry firend got lost?
                     <Link to='/posts' > Search</Link>
+
                 </div>
                 <div className="option3">
                     Did you find a lost pet?
                     <Link to='/posts' > Adopt</Link>
+                    <img src={Cat2} className="option3img" />
                 </div>
-            </div>
-            
-        </>
+                
+            </div>            
 
             
     )
